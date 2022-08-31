b8e9bc782b601fce69c786435655646bc66f8e68
nneddk <meljandre.mc@gmail.com>
2022-08-31 14:04:42 +0800

    skeleton script for weather data added

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//add animated list
+const animatedList = document.createElement('ul');
+    animatedList.classList.add('shapes');
+    for(let i = 0; i<10; i++){
+        const shapeDiv = document.createElement('li');
+        animatedList.appendChild(shapeDiv);
+    }
+
+document.body.appendChild(animatedList);
+
 //time set
 function startTime() {
     let today = new Date(), h = today.getHours(), m = today.getMinutes();
@@ -6,22 +16,31 @@ function startTime() {
     h = h ? h: h = 12;
     h = checkTime(h);
     m = checkTime(m);
-    document.getElementById('time-slot').textContent = h + ':' + m;
-    document.getElementById('am-or-pm').textContent = ampm;
+    document.getElementById('current-time').textContent = h + ':' + m;
+    document.getElementById('current-am-or-pm').textContent = ampm;
     setTimeout(startTime, 1000);
     
-    function checkTime(i) {
-        return (i < 10) ? '0' + i : i;
-    }
+    
+}
+function checkTime(i) {
+  return (i < 10) ? '0' + i : i;
 }
 //startTime();
 
 //search location
 const searchLocationBtn = document.getElementById('search-location-button');
-searchLocationBtn.onclick = function(){
-    const location = document.getElementById('location');
-    updateTemp(location);
+const searchLocation = document.getElementById('search-location');
+searchLocation.onkeydown = function(e){
+  if (e.key === 'Enter') {
+    updateTemp(searchLocation);
+  }
 }
+searchLocationBtn.onclick = function(){
+    
+    updateTemp(searchLocation);
+}
+
+
 
 //api call to openweathermap url:  https://openweathermap.org/
 
@@ -38,6 +57,32 @@ async function updateTemp(location) {
 
 function locationFound(data){
     console.log(data);
+    //city name, country
+    console.log('name: ', data.name);
+    console.log('country: ', data.sys.country);
+
+    //coordinates
+    console.log('long: ', data.coord.lon);
+    console.log('lat: ', data.coord.lat);
+
+    //temperature farenheit
+    console.log('temp: ', data.main.temp);
+    console.log('humidity: ', data.main.humidity);
+
+    //weather
+    console.log(data.weather[0].description);
+
+    //sunrise, sunset
+    let sunrise = new Date(data.sys.sunrise * 1000);
+    let sunset = new Date(data.sys.sunset * 1000);
+
+    console.log('sunrise: ', sunrise);
+    console.log('sunset: ', sunset);
+
+    //wind data
+    console.log(data.wind.deg);
+    console.log(data.wind.speed);
+    
 }
 
 function notFound(){
