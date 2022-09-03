5cb3eab2bdd491d2035f32cb18d6d91dd5114835
nneddk <meljandre.mc@gmail.com>
2022-09-03 18:00:04 +0800

    skeleton updated, functionality added

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+function mainPage(){
+    const content = document.createElement('div');
+    content.setAttribute('id', 'content');
+
+    const timeContent = document.createElement('div');
+    timeContent.setAttribute('id', 'time');
+
+    const timeSlot = document.createElement('div');
+    timeSlot.classList.add('time-slot');
+
+    const currentTime = document.createElement('div');
+    currentTime.setAttribute('id','current-time');
+
+    const currentAMorPM = document.createElement('div');
+    currentAMorPM.setAttribute('id','current-am-or-pm');
+
+    timeSlot.appendChild(currentTime);
+    timeSlot.appendChild(currentAMorPM);
+
+    const searchLocationSpan = document.createElement('span');
+    searchLocationSpan.classList.add('search-location-span');
+
+    const searchLocation = document.createElement('input');
+    searchLocation.setAttribute('type', 'search');
+    searchLocation.setAttribute('id', 'search-location');
+    searchLocation.setAttribute('autocomplete', 'off');
+    searchLocation.setAttribute('placeholder', 'search');
+
+    const searchLocationBtn = document.createElement('button');
+    searchLocationBtn.setAttribute('type', 'button');
+    searchLocationBtn.setAttribute('id', 'search-location-button');
+
+    searchLocation.onkeydown = function(e){
+      if (e.key === 'Enter') {
+        updateTemp(searchLocation);
+      }
+    }
+    searchLocationBtn.onclick = function(){
+        
+        updateTemp(searchLocation);
+    }
+
+    searchLocationSpan.appendChild(searchLocation);
+    searchLocationSpan.appendChild(searchLocationBtn);
+
+    timeContent.appendChild(timeSlot);
+    timeContent.appendChild(searchLocationSpan)
+
+    content.appendChild(timeContent);
+    
+    return content;
+}
+
+document.body.appendChild(mainPage());
+
+
 //add animated list
 const animatedList = document.createElement('ul');
     animatedList.classList.add('shapes');
@@ -10,35 +66,23 @@ document.body.appendChild(animatedList);
 
 //time set
 function startTime() {
+    const currentTime = document.getElementById('current-time');
+    const currentAMorPM = document.getElementById('current-am-or-pm');
     let today = new Date(), h = today.getHours(), m = today.getMinutes();
     let ampm = h > 12 ? 'pm': 'am';
     h = h % 12;
     h = h ? h: h = 12;
     h = checkTime(h);
     m = checkTime(m);
-    document.getElementById('current-time').textContent = h + ':' + m;
-    document.getElementById('current-am-or-pm').textContent = ampm;
-    setTimeout(startTime, 1000);
-    
+    currentTime!=null?currentTime.textContent = h + ':' + m:0;
+    currentAMorPM!=null?currentAMorPM.textContent = ampm:0;
+    setTimeout(startTime, 1000);  
     
 }
 function checkTime(i) {
   return (i < 10) ? '0' + i : i;
 }
-//startTime();
-
-//search location
-const searchLocationBtn = document.getElementById('search-location-button');
-const searchLocation = document.getElementById('search-location');
-searchLocation.onkeydown = function(e){
-  if (e.key === 'Enter') {
-    updateTemp(searchLocation);
-  }
-}
-searchLocationBtn.onclick = function(){
-    
-    updateTemp(searchLocation);
-}
+startTime();
 
 //data for testing
 let testData = {
@@ -67,13 +111,12 @@ let testData = {
 }
 
 //api call to openweathermap url:  https://openweathermap.org/
-
+//updateTemp();
 async function updateTemp(location) {
     const  currContent = document.getElementById('content');
     while (currContent.hasChildNodes()){
       currContent.removeChild(currContent.lastChild);
     }
-    
     const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+location.value+'&appid=eca5ec86a08e6a56a853ad00606412d8&units=imperial');
     const data = await response.json();
     
@@ -82,15 +125,15 @@ async function updateTemp(location) {
     } else {
       currContent.appendChild(notFound());
     }
-    
-  
+    /*
+    currContent.appendChild(locationFound(testData));
+  */
 
   }
 
 
 function locationFound(data){
 
-    console.log(data);
     const contentFound = document.createElement('div');
     contentFound.classList.add('content-found');
 
@@ -111,11 +154,6 @@ function locationFound(data){
     placeCountry.classList.add('place-country');
     placeCountry.textContent = data.sys.country;
 
-
-
-    console.log('name: ', data.name);
-    console.log('country: ', data.sys.country);
-
     //coordinates
 
     const placeLong = document.createElement('div');
@@ -128,8 +166,6 @@ function locationFound(data){
     placeLat.classList.add('place');
     placeLat.textContent = data.coord.lat;
     
-    console.log('long: ', data.coord.lon);
-    console.log('lat: ', data.coord.lat);
 
     //temperature farenheit
 
@@ -149,16 +185,13 @@ function locationFound(data){
     placeTemp.appendChild(placeTempNow);
     placeTemp.appendChild(placeHumid);
 
-    console.log('temp: ', data.main.temp);
-    console.log('humidity: ', data.main.humidity);
-
     //weather
 
     const placeWeather = document.createElement('div');
     placeWeather.classList.add('place-weather');
     placeWeather.classList.add('place');
-    placeWeather.textContent = data.weather[0].description;
-    console.log(data.weather[0].description);
+    placeWeather.textContent = data.weather[0].id;
+
 
     placeData.appendChild(placeName);
     placeData.appendChild(placeCountry);
@@ -175,9 +208,20 @@ function locationFound(data){
     //sunrise, sunset
     let sunrise = new Date(data.sys.sunrise * 1000);
     let sunset = new Date(data.sys.sunset * 1000);
+  
+    function setTime(date){
+      let hours = date.getHours(), minutes = date.getMinutes();
+      let amorpm = hours > 12 ? 'pm':'am';
+      hours = hours % 12;
+      hours = hours ? hours : hours = 12;
 
-    console.log('sunrise: ', sunrise);
-    console.log('sunset: ', sunset);
+      hours = checkTime(hours);
+      minutes = checkTime(minutes);
+
+      return hours+':'+minutes+' '+amorpm;
+      
+    }
+
     //sunrise
     const placeSunrise = document.createElement('div');
     placeSunrise.classList.add('place-sunrise');
@@ -187,8 +231,9 @@ function locationFound(data){
     placeSunrisePic.classList.add('place-sunrise-pic');
 
     const placeSunriseTime = document.createElement('div');
+    placeSunriseTime.classList.add('place-sunrise-time');
     placeSunriseTime.classList.add('place');
-    placeSunriseTime.textContent = sunrise;
+    placeSunriseTime.textContent = setTime(sunrise);
 
     placeSunrise.appendChild(placeSunrisePic);
     placeSunrise.appendChild(placeSunriseTime);
@@ -201,8 +246,9 @@ function locationFound(data){
     placeSunsetPic.classList.add('place-sunset-pic');
 
     const placeSunsetTime = document.createElement('div');
+    placeSunsetTime.classList.add('place-sunset-time');
     placeSunsetTime.classList.add('place');
-    placeSunsetTime.textContent = sunset;
+    placeSunsetTime.textContent = setTime(sunset);
 
     placeSunset.appendChild(placeSunsetPic);
     placeSunset.appendChild(placeSunsetTime);
@@ -213,8 +259,6 @@ function locationFound(data){
     
 
     //wind data
-    console.log(data.wind.deg);
-    console.log(data.wind.speed);
 
     const placeWind = document.createElement('div');
     placeWind.classList.add('place-wind');
@@ -246,6 +290,16 @@ function locationFound(data){
     returnBack.setAttribute('type', 'button');
     returnBack.classList.add('return-back');
     returnBack.textContent = 'return home';
+
+    returnBack.onclick = function(){
+      const  currContent = document.getElementById('content');
+      while (currContent.hasChildNodes()){
+        currContent.removeChild(currContent.lastChild);
+      }
+
+      currContent.appendChild(mainPage());
+      startTime();
+    }
 
     sideB.appendChild(placeSunrise);
     sideB.appendChild(placeSunset);
@@ -303,7 +357,7 @@ window.addEventListener("resize", function () {
   // check width and reset width so mobile view rotate doesnt mess up
   const sideA = document.querySelector('.side-a');
   const sideB = document.querySelector('.side-b');
-  sideA.style.width = '';
-  sideB.style.width = '';
+  sideA!=null?sideA.style.width = '':0;
+  sideB!=null?sideB.style.width = '':0;
  
 });
